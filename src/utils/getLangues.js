6fb6f2fbc391c458a7c100f4557d912ebc17f7f1
= <=>
2024-03-18 15:41:27 +0100

    bug fix import pour linux

--- src/utils/getLangues.js
+++ src/utils/getLangues.js
@@ -1,13 +1,15 @@
 const fs = require("node:fs")
 const os = require('node:os')
 
+let osSymbol = '/'
+if(os.platform() === "win32") {
+    osSymbol = "\\"
+}
+
 module.exports = () => {
-    let symbol;
-    if(os.platform() === "darwin") symbol = "/"
-    if(os.platform() === "win32") symbol = "\\"
-    let path = require.resolve("../langues/eng.json").split(symbol)
+    let path = require.resolve("../langues/eng.json").split(osSymbol)
     path.pop()
-    path = path.join(symbol)
+    path = path.join(osSymbol)
     let files = fs.readdirSync(path).filter(e => e.endsWith("json")).map(e => require(`../langues/${e}`)).filter(e => e["languageCode"] && e["commands"] && e["options"] && e["choices"])
     return files
 }
